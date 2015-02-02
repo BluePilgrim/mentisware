dc2919f7e657aa4c6eff766c3abcf281a922a4f0
BluePilgrim <david.yang@mentisware.com>
2015-02-02 09:33:29 -0800

    added tests for min heap

--- Algorithm Study/src/com/mentisware/sort/HeapTest.scala
+++ Algorithm Study/src/com/mentisware/sort/HeapTest.scala
@@ -8,7 +8,7 @@ class HeapTest extends UnitSpec{
   def testSet1 = (
       List(4, 1, 3, 2, 16, 9, 10, 14, 8, 7),
       List(16, 14, 10, 8, 7, 9, 3, 2, 4, 1),
-      Some(16)
+      Some(16), Some(1)
   )
 
   def testSet2: List[Int] = {
@@ -29,7 +29,7 @@ class HeapTest extends UnitSpec{
     (emptyHeap.extractHead) should equal (None)
   }
 
-  val (xs, ys, max) = testSet1
+  val (xs, ys, max, min) = testSet1
   val heap = new Heap[Int](xs)
 
   "A heap" should "be created from a unsorted list" in {
@@ -41,15 +41,31 @@ class HeapTest extends UnitSpec{
     (heap.extractHead) should equal (max)
     (heap.getLength) should equal (len - 1)
   }
+  
+  "A min heap" should "be able to extract min reducing the size by one" in {
+    val minHeap = new Heap[Int](xs, false)
+    val len = minHeap.getLength
+    
+    (minHeap.extractHead) should equal (min)
+    (minHeap.getLength) should equal (len - 1)
+  }
 
   
   "A blank heap" should "constructed from add with same result as a new heap with the list" in {
     val xs = testSet2
-    val heap1 = new Heap[Int](xs)
-    val heap2 = new Heap[Int](Nil)
+    val maxHeap1 = new Heap[Int](xs)
+    val maxHeap2 = new Heap[Int](Nil)
     
-    xs foreach heap2.add
+    xs foreach maxHeap2.add
     
-    (heap1.getContents) should equal (heap2.getContents)
-  }
+    (maxHeap1.getContents) should equal (maxHeap2.getContents)
+
+    val minHeap1 = new Heap[Int](xs, false)
+    val minHeap2 = new Heap[Int](Nil, false)
+    
+    xs foreach minHeap2.add
+    
+    (minHeap1.getContents) should equal (minHeap2.getContents)
+    (minHeap1.extractHead) should equal (minHeap2.extractHead)
+}
 }
