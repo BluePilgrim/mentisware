73f4454226d3611bd9ca272c8c2faff8b78565ba
BluePilgrim <david.yang@mentisware.com>
2015-02-21 13:25:02 -0800

    Refactored the package structure and code of tree's. Now, SearchTree comes as the representative. RBTree is in com.mentisware.tree.rbtree.

---
+++ Algorithm Study/src/com/mentisware/tree/rbtree/OrderingTree.scala
@@ -0,0 +1,27 @@
+package com.mentisware.tree.rbtree
+
+import com.mentisware.tree._
+
+class OrderingTree[T : Ordering](
+    elem: Element[T],
+    left: RBTree[T],
+    right: RBTree[T],
+    color: Color,
+    nil: NilTree[T]
+    ) extends NormalTree[T](elem, left, right, color, nil) {
+  override val size = left.size + right.size + 1
+  override def createNode(e: Element[T], l: RBTree[T], r: RBTree[T], c: Color): RBTree[T] = new OrderingTree(e, l, r, c, nil)
+}
+
+
+object OrderingTree {
+  def build[T : Ordering](xs: List[Element[T]]): RBTree[T] = {
+    val nil = new NilTree[T]
+    if (xs.length == 0) nil
+    else {
+      val tree: RBTree[T] = new OrderingTree[T](xs.head, nil, nil, Black, nil)
+      
+      (tree /: xs.tail) { _.insert(_) }
+    }
+  }
+}
---
+++ Algorithm Study/src/com/mentisware/tree/rbtree/RBTreeTest.scala
@@ -0,0 +1,127 @@
+package com.mentisware.tree.rbtree
+
+import com.mentisware.tree._
+
+import com.mentisware.test.UnitSpec
+import com.mentisware.sort.Orderer
+
+case class IntElem (key: Int) extends Element[Int]
+
+
+trait RBTreeBehavior { this: UnitSpec =>
+  def performPrimitiveOperations(xs: List[Int], tree: RBTree[Int]) {
+    val es = xs.map(IntElem(_))
+    
+    
+    it should "return the minimum number" in {
+      (tree.minimum) should equal (Some(IntElem(Orderer.selectSmallest(xs, 1))))
+    }
+    
+    it should "return the maximum number" in {
+      (tree.maximum) should equal (Some(IntElem(Orderer.selectSmallest(xs, xs.length))))
+    }
+    
+    it should "pass the RB property validation after insert" in {
+      tree.validateRBProperty()
+    }
+
+    it should "pass the RB property validation after delete" in {
+      tree.delete(es.head).delete(es.tail.head).validateRBProperty()
+    }
+    
+    it should "return NilTree after deletion of all elements" in {
+      val result = (tree /: es) { (t, e) =>
+//        println("Delete " + e.key + " from\n" + t)
+        t.validateRBProperty()
+        t.delete(e)
+      }
+ 
+      (result.isEmpty()) should equal (true)
+    }
+  }
+  
+  def performComplexOperations(xs: List[Int], tree: RBTree[Int]) {
+    val es = xs.map(IntElem(_))
+    val len = xs.length
+
+    var t = tree
+
+    it should "delete a half of elements" in {
+      for (i <- 0 until len by 2) t = t.delete(es(i))
+      t.validateRBProperty()
+    }
+
+    it should "add a half of the deleted elements in reverse order" in {
+      for (i <- (0 until len/2 by 2).reverse) t = t.insert(es(i))
+      t.validateRBProperty()
+    }
+  }
+  
+  def processOrderingStatistics(xs: List[Int], tree: RBTree[Int]) {
+    val es = xs.map(IntElem(_))
+    val len = xs.length
+    val rnd = new scala.util.Random(System.currentTimeMillis())
+    
+    it should "select random orders correctly" in {
+      for (i <- 1 to len/2) {
+        val order = rnd.nextInt(len) + 1    // nextInt exclude len
+        (tree.selectSmallest(order).key) should equal (Orderer.selectSmallest(xs, order))
+      }
+    }
+    
+    it should "return correct rank information of random data in test set" in {
+      // we should take care of multiple identical keys
+      for (i <- 1 to len/2) {
+        val order = rnd.nextInt(len) + 1
+        val node = tree.selectSmallest(order)
+//        println("order = " + order + " with node key = " + node.key)
+//        (node.rankIn(tree)) should equal (order)
+        (tree.selectSmallest(node.rankIn(tree)).key) should equal (node.key)
+      }
+    }
+  } 
+}
+
+
+class RBTreeTest extends UnitSpec with RBTreeBehavior {
+  def determinedSet = {
+
+    val xs = List(3, 11, 10, -1, -45, 2, -4, 7, -5, 9, 1, 100, 2, 4, -100, 98, 46)
+    
+    (xs,
+    RBTree.build[Int](xs.map(IntElem(_))),
+    OrderingTree.build[Int](xs.map(IntElem(_)))
+    )
+  }
+
+  def randomSet(size: Int = 1000) = {
+    val rnd = new scala.util.Random(System.currentTimeMillis())
+    val xs = List.fill(size)(rnd.nextInt(100000))
+    
+    (xs,
+    RBTree.build[Int](xs.map(IntElem(_))),
+    OrderingTree.build[Int](xs.map(IntElem(_)))
+    )
+  }
+
+  val detSet = determinedSet
+  val rndSet = randomSet(10000)
+
+  "Determined Set with Red Black Tree" should behave like performPrimitiveOperations(detSet._1, detSet._2)
+  it should behave like performComplexOperations(detSet._1, detSet._2)
+  it should behave like processOrderingStatistics(detSet._1, detSet._2)
+
+  "Random Set with Red Black Tree" should behave like performPrimitiveOperations(rndSet._1, rndSet._2)
+  it should behave like performComplexOperations(rndSet._1, rndSet._2)
+  it should behave like processOrderingStatistics(rndSet._1, rndSet._2)
+  
+
+  "Determined Set with Ordering Tree" should behave like performPrimitiveOperations(detSet._1, detSet._3)
+  it should behave like performComplexOperations(detSet._1, detSet._3)
+  it should behave like processOrderingStatistics(detSet._1, detSet._3)
+
+  "Random Set with Ordering Tree" should behave like performPrimitiveOperations(rndSet._1, rndSet._3)
+  it should behave like performComplexOperations(rndSet._1, rndSet._3)
+  it should behave like processOrderingStatistics(rndSet._1, rndSet._3)
+
+}
