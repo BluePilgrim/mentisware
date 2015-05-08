9e021d07cf9b947de920a82db8a9c5c24873a3c1
BluePilgrim <david.yang@mentisware.com>
2015-05-07 23:24:20 -0700

    Enabled the weighted-union heuristic check for ForestSet

--- Algorithm Study/src/com/mentisware/disjointset/ForestSetTest.scala
+++ Algorithm Study/src/com/mentisware/disjointset/ForestSetTest.scala
@@ -8,12 +8,15 @@ trait ForestSetTestSet {
 
 class ForestSetTest extends UnitSpec with DisjointSetBehavior with ForestSetTestSet {
   "Forest Set" should behave like maintainSetsCorrectly(set1)(ForestSet.apply[Int])
-/*
+
   it should "perform union with weight-union heuristic" in {
-    val djset = ForestSet.build(List(1 to 6, 7 to 10))
+    val djset = ForestSet(set1)
+    djset.union(1, 2).union(1, 3).union(1, 4).union(7, 8)
+
     val rep1 = djset.set(1)
     val rep2 = djset.set(7)
-    (djset.union(1, 7).set(1).representative) should equal (djset.set(7).representative)
+    djset.union(1, 7)
+    (rep1.representative) should equal (djset.set(7).representative)
   }
-*/
+
 }
